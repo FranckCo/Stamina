862db15fb73b1a6cbb906a7a83e0fe45d8eb9ffe
Franck <franck.cotton@gmail.com>
2016-02-21 22:21:45 +0100

    Express serving app (sort of)

--- src/js/server/server.js
+++ src/js/server/server.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import path from 'path';
 
 const SERVER_PORT = 3000;
 
 var server = express();
 
-server.get('/', function (request, response) {
-  response.send('Hello World!');
+server.get('/build/build.min.js', function (request, response) {
+  response.sendFile(path.resolve(__dirname, '../../../dist/build/build.min.js'));
+});
+
+server.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, '../../../dist/index.html'));
 });
 
 server.listen(SERVER_PORT, () => {
