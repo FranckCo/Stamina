411fa58303ef079a62756d6161cd16e3ca1edc12
Franck <franck.cotton@gmail.com>
2016-03-31 09:35:53 +0200

    Modified XKOS.java to add new class and properties

--- src/main/java/fr/insee/stamina/utils/XKOS.java
+++ src/main/java/fr/insee/stamina/utils/XKOS.java
@@ -39,45 +39,48 @@ public class XKOS {
 	 * Defines XKOS Properties
 	   ########################################################## */
 	// XKOS annotation properties (sub-properties of skos:note)
-	public static final Property inclusionNote = model.createProperty( uri + "inclusionNote");
-	public static final Property coreContentNote = model.createProperty( uri + "coreContentNote");
-	public static final Property additionalContentNote = model.createProperty( uri + "additionalContentNote");
-	public static final Property exclusionNote = model.createProperty( uri + "exclusionNote");
-	public static final Property caseLaw = model.createProperty( uri + "caseLaw"); // Not yet in the official draft
+	public static final Property inclusionNote = model.createProperty(uri + "inclusionNote");
+	public static final Property coreContentNote = model.createProperty(uri + "coreContentNote");
+	public static final Property additionalContentNote = model.createProperty(uri + "additionalContentNote");
+	public static final Property exclusionNote = model.createProperty(uri + "exclusionNote");
+	public static final Property caseLaw = model.createProperty(uri + "caseLaw"); // Not yet in the official draft
 	// XKOS data properties
-	public static final Property numberOfLevels = model.createProperty( uri + "numberOfLevels");
-	public static final Property depth = model.createProperty( uri + "depth");
-	public static final Property maxLength = model.createProperty( uri + "maxLength");
+	public static final Property numberOfLevels = model.createProperty(uri + "numberOfLevels");
+	public static final Property depth = model.createProperty(uri + "depth");
+	public static final Property maxLength = model.createProperty(uri + "maxLength");
+	public static final Property notationPattern = model.createProperty(uri + "notationPattern");
+	public static final Property plainText = model.createProperty(uri + "plainText");
 	// XKOS object properties
-	public static final Property levels = model.createProperty( uri + "levels");
-	public static final Property madeOf = model.createProperty( uri + "madeOf");
-	public static final Property sourceConcept = model.createProperty( uri + "sourceConcept");
-	public static final Property targetConcept = model.createProperty( uri + "targetConcept");
-	public static final Property follows = model.createProperty( uri + "follows");
-	public static final Property supersedes = model.createProperty( uri + "supersedes");
-	public static final Property variant = model.createProperty( uri + "variant");
-	public static final Property belongsTo = model.createProperty( uri + "belongsTo");
-	public static final Property organizedBy = model.createProperty( uri + "organizedBy");
-	public static final Property classifiedUnder = model.createProperty( uri + "classifiedUnder");
+	public static final Property levels = model.createProperty(uri + "levels");
+	public static final Property madeOf = model.createProperty(uri + "madeOf");
+	public static final Property sourceConcept = model.createProperty(uri + "sourceConcept");
+	public static final Property targetConcept = model.createProperty(uri + "targetConcept");
+	public static final Property compares = model.createProperty(uri + "compares");
+	public static final Property follows = model.createProperty(uri + "follows");
+	public static final Property supersedes = model.createProperty(uri + "supersedes");
+	public static final Property variant = model.createProperty(uri + "variant");
+	public static final Property belongsTo = model.createProperty(uri + "belongsTo");
+	public static final Property organizedBy = model.createProperty(uri + "organizedBy");
+	public static final Property classifiedUnder = model.createProperty(uri + "classifiedUnder");
 	// XKOS coverage properties (object properties)
-	public static final Property covers = model.createProperty( uri + "covers");
-	public static final Property coversExhaustively = model.createProperty( uri + "coversExhaustively");
-	public static final Property coversMutuallyExclusively = model.createProperty( uri + "coversMutuallyExclusively");
+	public static final Property covers = model.createProperty(uri + "covers");
+	public static final Property coversExhaustively = model.createProperty(uri + "coversExhaustively");
+	public static final Property coversMutuallyExclusively = model.createProperty(uri + "coversMutuallyExclusively");
 	// XKOS semantic relations properties
-	public static final Property causal = model.createProperty( uri + "causal");
-	public static final Property causes = model.createProperty( uri + "causes");
-	public static final Property causedBy = model.createProperty( uri + "causedBy");
-	public static final Property sequential = model.createProperty( uri + "sequential");
-	public static final Property precedes = model.createProperty( uri + "precedes");
-	public static final Property previous = model.createProperty( uri + "previous");
-	public static final Property succeeds = model.createProperty( uri + "succeeds");
-	public static final Property next = model.createProperty( uri + "next");
-	public static final Property temporal = model.createProperty( uri + "temporal");
-	public static final Property before = model.createProperty( uri + "before");
-	public static final Property after = model.createProperty( uri + "after");
-	public static final Property isPartOf = model.createProperty( uri + "isPartOf");
-	public static final Property hasPart = model.createProperty( uri + "hasPart");
-	public static final Property specializes = model.createProperty( uri + "specializes");
-	public static final Property generalizes = model.createProperty( uri + "generalizes");
-	public static final Property disjoint = model.createProperty( uri + "disjoint");
+	public static final Property causal = model.createProperty(uri + "causal");
+	public static final Property causes = model.createProperty(uri + "causes");
+	public static final Property causedBy = model.createProperty(uri + "causedBy");
+	public static final Property sequential = model.createProperty(uri + "sequential");
+	public static final Property precedes = model.createProperty(uri + "precedes");
+	public static final Property previous = model.createProperty(uri + "previous");
+	public static final Property succeeds = model.createProperty(uri + "succeeds");
+	public static final Property next = model.createProperty(uri + "next");
+	public static final Property temporal = model.createProperty(uri + "temporal");
+	public static final Property before = model.createProperty(uri + "before");
+	public static final Property after = model.createProperty(uri + "after");
+	public static final Property isPartOf = model.createProperty(uri + "isPartOf");
+	public static final Property hasPart = model.createProperty(uri + "hasPart");
+	public static final Property specializes = model.createProperty(uri + "specializes");
+	public static final Property generalizes = model.createProperty(uri + "generalizes");
+	public static final Property disjoint = model.createProperty(uri + "disjoint");
 }
