1306400230ff204db3a02ba43cc2b1aa4aceb5c1
Franck <franck.cotton@gmail.com>
2016-05-19 10:42:41 +0200

    Group ISIC and CPC model makers in one package

--- src/main/java/fr/insee/stamina/unsd/CPCModelMaker.java
+++ src/main/java/fr/insee/stamina/unsd/CPCModelMaker.java
@@ -1,4 +1,4 @@
-package fr.insee.stamina.cpc;
+package fr.insee.stamina.unsd;
 
 import java.io.File;
 import java.io.FileInputStream;
--- src/main/java/fr/insee/stamina/unsd/ISICModelMaker.java
+++ src/main/java/fr/insee/stamina/unsd/ISICModelMaker.java
@@ -1,4 +1,4 @@
-package fr.insee.stamina.isic;
+package fr.insee.stamina.unsd;
 
 import java.io.File;
 import java.io.FileInputStream;
